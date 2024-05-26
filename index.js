b61d68620cad5df27d4fd7717abfe7ba92f4c016
Oltiariq3561 <oltiariq356@gmail.com>
2024-05-26 15:55:19 -0700

    push

---
+++ index.js
@@ -0,0 +1,168 @@
+// 1-masala
+
+// let a = + prompt("odd");
+
+// for(let i = 1; i<=a; i++ ){
+// console.log(i);
+// }
+
+
+// 2-masala
+
+// let a = + prompt("odd");
+
+// for(let i = a; i<=1; i++ ){
+// console.log(i);
+// }
+
+
+// 3-masala
+// let n = + prompt("son")
+
+// let s=0;
+
+// for(let i =1; i<= n; i++) {
+// s = s + i;
+
+// }
+// console.log(s)
+
+// 4-masala
+
+// let n = + prompt("son")
+
+// let s=0;
+
+// for(let i =1; i<= n; i++) {
+//     if(i%2==0){
+//         s=s+i;
+
+// }
+// }
+// console.log(s);
+
+
+// 5-masala
+
+// let n = + prompt("son")
+
+// let s=0;
+
+// for(let i =1; i<= n; i++) {
+//     if(i%2==1){
+//         s=s+i;
+
+// }
+// }
+// console.log(s);
+
+
+
+// 6-masala
+
+// let n = + prompt("son")
+
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+//     console.log(i);
+//     }
+// }
+
+
+// 7-masala
+
+// let n = + prompt("son")
+
+// let count=0
+
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+
+//         count++
+
+//         console.log(count);
+//     }
+    
+// }
+
+
+// 8-masala
+
+// let n = + prompt("son");
+
+// let count = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     if (n%i==0){
+// count++
+//     }
+    
+// }
+
+// if(count == 2){
+// console.log("tub");
+// }else{
+// console.log("tub emas");
+// }
+
+
+
+// for 4 
+
+// let a = + prompt("narx");
+
+// for (let i=1; i<=10; i++){
+// console.log(a*i);
+// }
+
+
+// for 5
+
+// let a = + prompt("narx");
+
+// for (let i=0.1; i<=1; i+0.1){
+// console.log(a*i);
+// }
+
+
+// for6
+
+// let a = + prompt("narx");
+
+// for (let i=1; i<=1; i+0.2){
+// console.log(a*i);
+// }
+
+
+// for7
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let s=0;
+
+// for(let i =a; i<= b; i++) {
+// s = s + i;
+
+// }
+// console.log(s);
+
+
+// for8
+
+// let a = + prompt("son");
+
+// let b = + prompt("son");
+
+// let s=1;
+
+// for(let i =a; i<= b; i++) {
+// s = s * i;
+
+// }
+// console.log(s);
+
+
+// for9
+
